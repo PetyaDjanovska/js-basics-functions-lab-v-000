099616d332e6072651b3b7f053a443562d858eb4
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-30 16:23:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function calculatesFarePrice(start, end) {
   if (distance <= 400) {
     return 0
   } else if (distance > 400 && distance <= 2000){
-    
+    return ()(distance - 400) * 0.02)
   }
 }
